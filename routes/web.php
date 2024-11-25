30acc2b2fde5263afe134ac8995e847eac5a744b
JackFryer <210070237@aston.ac.uk>
2024-11-25 17:29:51 +0000

    force all files into views and comments on how to run locally

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,16 @@ route ::get('/aboutUs' , function(){
     return view('aboutUs');
 
 });
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+//To run locally you must go to gitbash, navigate to the project directory in
+//cd C:\xampp\htdocs\dashboard\'whateveryourfileiscalled'
+//start apache and mysql in xammp then in gitbash type php artisan serve
+//this will then run locally and you can then test changes
