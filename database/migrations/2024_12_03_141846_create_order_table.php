7ad7f03c5355a2fa3c2830eb0a2f85e21cac6ec7
TheVillian777 <jaghayer89@gmail.com>
2024-12-03 22:37:56 +0000

    Changes + New Migrations
    Added Migrations for Stock and Customer
    
    Changed details in Author, Category and Order migrations

---
+++ database/migrations/2024_12_03_141846_create_order_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Customer::class)->index();
+            $table->date('order_date');
+            $table->char('order_status', length:15);
+            $table->text('order_address');
+            $table->decimal('order_total_price', total: 10, places: 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order');
+    }
+};
