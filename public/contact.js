fec6342b897e57b75d0d3d2f43397c3c4dfcee38
SamirMohamud <220170279@aston.ac.uk>
2024-12-05 23:27:02 +0000

    Added a pop up to let the users know their request was submitted
    Added a pop up to let the users know their request was submitted

---
+++ public/contact.js
@@ -0,0 +1,5 @@
+
+// event listener submition 
+document.querySelector('.contact-form').addEventListener('submit', function(event) {
+    alert('Thank you for reaching out! Your request was successfully submitted and we will get back to you soon.');
+});
