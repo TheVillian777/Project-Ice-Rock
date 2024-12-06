96c67aff599e959d7b751b808ceab299205b64c6
Ellis <ellisimo03@gmail.com>
2024-12-06 00:55:52 +0000

    basket page
    did basket page and added options

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -3,12 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< Updated upstream
     <title>PageTurner</title>
     <link rel="stylesheet" href="styles.css">
+=======
+    <title>Basket | Page Turner</title>
+    <link rel="icon" href="logo.jpg" type="image/jpeg">
+    <link rel="stylesheet" href="basket_saved.css" onerror="alert('CSS file not found!')">
+>>>>>>> Stashed changes
 </head>
 <body>
 
     <!-- Logo -->
+<<<<<<< Updated upstream
     <header>
         <div class="title">
             <h1>PageTurner</h1>
@@ -45,5 +52,52 @@
       </h2>
 </div>
 
+=======
+    <header class="basket-Header">
+        <div class="logo">
+        <img src="logo.jpg" alt="Logo">
+        <h1>Basket</h1>
+        </div>
+    </header>
+    
+    <a href="shop" class="back-to-shopping"> < Back to Shopping</a>
+    <div class="left-column">
+    <div class="delivery-options">
+        <h2>Delivery Options</h2>
+        <form class="delivery-form">
+            <label>
+                <input type="radio" name="delivery" value="next-day"> 
+                Next Day (Orders before 6pm) - £5.99
+            </label>
+            <label>
+                <input type="radio" name="delivery" value="two-three-days"> 
+                2/3 Days - £2.99
+            </label>
+            <label>
+                <input type="radio" name="delivery" value="free-delivery"> 
+                (5/7 Days) - Free
+            </label>
+        </form>
+    </div>
+
+    <div class="promotion-code">
+        <h2>Promotion Code (if available)</h2>
+        <form class="promo-form">
+            <input type="text" placeholder="Enter your code" class="promo-input">
+            <button type="button" class="promo-apply">Apply</button>
+        </form>
+    </div>
+
+    <div class="your-order">
+            <h2>Your Order</h2>
+            <p>0 items</p>
+            <p>Subtotal (inc VAT): £0.00</p>
+            <hr>
+            <p class="total-to-pay">Total to Pay: £0.00</p>
+            <p>Total VAT: £0.00</p>
+        </div>
+    </div>
+</div>
+>>>>>>> Stashed changes
 </body>
 </html>
