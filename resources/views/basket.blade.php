9dc13c8c824bb83760f83fca66aab89feeaa92de
220038223 <220038223@aston.ac.uk>
2024-12-06 02:43:36 +0000

    basket-card
    added basket card, fully functional but need to make buttons change input (for mobile accessibility)

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -13,19 +13,43 @@
     <div class="main-container">
             <div class="basket-container">
                 <h2>basket:</h2>
+
                 <!-- basket card (book cover, title, author, quantity with arrows, price) -->
                 <div class="basket-card">
-                    <h2>item in basket</h2>
+                    <!-- cover -->
+                    <img src="book1.jpg" alt="Book Cover" class="book-cover">
+                    <!-- title, author -->
+                    <div class="book-info">
+                        <p class="title">Book Title</p>
+                        <p class="author">Author Name</p>
+                    </div>
+                    <div class="spacer"></div>
+                    <!-- price -->
+                    <p class="price">£8</p>
+                    <!-- qty -->
+                    <div class="quantity">
+                        <button class="arrow down">-</button>
+                        <input type="number" value="1" min="1" class="quantity-input">
+                        <button class="arrow up">+</button>
+                    </div>
                 </div>
+                
                 <div class="basket-card">
-                    <h2>item in basket</h2>
+                    <img src="book2.jpg" alt="Book Cover" class="book-cover">
+                    <div class="book-info">
+                        <p class="title">Book Title</p>
+                        <p class="author">Author Name</p>
+                    </div>
+                    <div class="spacer"></div>
+                    <p class="price">£8</p>
+                    <div class="quantity">
+                        <button class="arrow down">-</button>
+                        <input type="number" value="1" min="1" class="quantity-input">
+                        <button class="arrow up">+</button>
+                    </div>
                 </div>
-                <div class="basket-card">
-                    <h2>item in basket</h2>
-                </div>
-                <div class="basket-card">
-                    <h2>item in basket</h2>
-                </div>
+
+
             </div>
             <div class="checkout-container">
                 <h2>checkout:</h2>
