7fb39364e3f9b9ea8f6de1c10cb3b3d0d0b556c6
JackFryer <210070237@aston.ac.uk>
2025-02-04 18:19:43 +0000

    start to forgotten password

--- public/contact.js
+++
@@ -1,5 +0,0 @@
-
-// event listener submition 
-document.querySelector('.contact-form').addEventListener('submit', function(event) {
-    alert('Thank you for reaching out! Your request was successfully submitted and we will get back to you soon.');
-});
