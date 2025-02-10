7a87f4fed862588151bfe9e45e2ffd26bc0ee983
pnanksonani <pinanksonani0415@gmail.com>
2025-02-10 20:43:52 +0000

    Update aboutUs.blade.php

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AboutUs</title>
-    <link rel="stylesheet" href="aboutUs.css">
-    <script src="aboutUs.js" defer></script>
+    <link rel="stylesheet" href="css/aboutUs.css">
+    <script src="js/aboutUs.js" defer></script>
+
 </head>
 <body>
     <!-- Header -->
