4614e8505a85a71eb7a8f9923c9726d158f22f11
pnanksonani <pinanksonani0415@gmail.com>
2025-02-11 04:04:06 +0000

    Update aboutUs.blade.php

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -44,8 +44,9 @@
         <!--<a href="{{ route('saved') }}">Saved</a>-->
         <a href="{{ route('basket') }}">Basket</a>
         <a href="{{ route('login') }}">Profile</a>
-        <a href="{{ route('contact') }}">Contact Us</a>
         <a class="active" href="{{ route('aboutUs') }}">About Us</a>
+        <a href="{{ route('contact') }}">Contact Us</a>
+        
 
         @if (Auth::check())
         <form action="{{ route('logout')}}" method="POST">
