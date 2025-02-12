a5a8a4891483c3b4fbf514caa4810325ea881eb2
Ellis <ellisimo03@gmail.com>
2025-02-12 01:49:15 +0000

    working on profile page
    profile page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,10 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
-    <link rel="stylesheet" href="contact.css" onerror="alert('CSS file not found!')">
+    <link rel="stylesheet" href="profile.css" onerror="alert('CSS file not found!')">
     <script src="contact.js" defer></script>
 </head>
 <body>
+
+<div class="search-box">
+        <form action="{{ route('shopSearch') }}" method="POST">
+            @csrf
+            <div class="search-bar">
+                <input type="text" name='search' placeholder="search for books..." id="search" value="{{ request()->input('search') }}">
+                <button type="submit"><img src="magnifying-glass.png" alt="Search" class="search-icon"></button>
+            </div>   
+        </form>
+        <!--<input type="text" placeholder="Search for books..." id="search-bar">
+        <img src="magnifying-glass.png" alt="Search" class="search-icon">-->
+
+    </div>
+
+    <!-- Navigation Bar -->
+    <div class="navBar">
+        <a class="active" href="{{ route('index') }}">Home</a>
+        <a href="{{ route('shop') }}">Books</a>
+        <!--<a href="{{ route('saved') }}">Saved</a>-->
+        <a href="{{ route('basket') }}">Basket</a>
+        <a href="{{ route('profile') }}">Profile</a>
+        <a href="{{ route('aboutUs') }}">About Us</a>
+        <a href="{{ route('contact') }}">Contact Us</a>
+        <a href="{{ route('profile') }}">Profile</a>
+        @if (Auth::check())
+        <form action="{{ route('logout')}}" method="POST">
+            @csrf
+        <button type="submit">Log Out</button>
+        </form>
+        @else 
+        <a href="{{ route('login') }}">Login</a>
+        </form>
+        @endif
+    </div>
+
     <!-- Possible features of Profile Page -->
 
     <!-- 1. Username
