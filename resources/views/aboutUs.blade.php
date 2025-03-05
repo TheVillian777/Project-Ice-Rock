595ce390448c434f3276fef82c3e8cf5a314d143
rali1x <220111847@aston.ac.uk>
2025-03-05 12:54:19 +0000

    AboutUs-Fixes
    Banner Added

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -9,63 +9,104 @@
     <script src="{{ asset('js/aboutus.js') }}" defer></script>
 
     <title>PageTurner</title>
-    <link rel="stylesheet" href="css/styles.css" onerror="alert('CSS file not found!')">
 
 </head>
 
 <body>
-    <!-- Header -->
-    <header>
+<header class="new-header">
         <div class="logo">
-            <img src="logo.png" alt="Logo">
+            <img src="images/logo.png" alt="Logo">
+            <h1>PageTurner</h1>
         </div>
 
+        <div class="search-box">
+            <form action="{{ route('shopSearch') }}" method="POST">
+                @csrf
+                <div class="search-bar">
+                    <input type="text" name='search' placeholder="Search for books..." id="search" value="{{ request()->input('search') }}">
+                    <button type="submit" class="search-icon">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
+            </form>
+        </div>
 
-        <div class="title">
+        <div class="header-right">
+            <div class="account-dropdown">
+                <a href="#" class="account-button">
+                    <i class="fa fa-user"></i> ACCOUNT
+                </a>
+                <div class="account-dropdown-content">
+                    <form action="{{ route('login') }}" method="GET">
+                        @csrf
+                        <button type="submit">Sign In</button>
+                    </form>
+                    <div class="dropdown-divider"></div>
+                    <form action="{{ route('register') }}" method="GET">
+                        @csrf
+                        <button type="submit">Register</button>
+                    </form>
+                </div>
+            </div>
+            <!-- Wishlist Button -->
+            <div class="wishlist-button">
+                <a href="{{ route('saved')}}">
+                   <i class="fa fa-heart"></i>  WISHLIST
+                </a>
+            </div>
 
-            <h1>PageTurner</h1>
+            <div class="basket-button-container">
+                <a href="{{ route('basket') }}" class="basket-button">
+                    <i class="fa fa-shopping-basket"></i> £0.00
+                </a>
+            </div>
 
         </div>
     </header>
 
-    <!-- Search Bar -->
-    <h1>
-        <div class="logo">
-            <img src="images/logo.png" alt="Logo">
-        </div>
-    </h1>
-
-    <!-- Search bar -->
-    <div class="search-box">
-        <form action="{{ route('shopSearch') }}" method="POST">
-            @csrf
-            <div class="search-bar">
-                <input type="text" name='search' placeholder="search for books..." id="search" value="{{ request()->input('search') }}">
-                <button type="submit"><img src="magnifying-glass.png" alt="Search" class="search-icon"></button>
-            </div>   
-        </form>
-    
-    </div>
-
     <!-- Navigation Bar -->
     <div class="navBar">
-        <a href="{{ route('index') }}">Home</a>
-        <a href="{{ route('shop') }}">Books</a>
-        <!--<a href="{{ route('saved') }}">Saved</a>-->
-        <a href="{{ route('basket') }}">Basket</a>
-        <a href="{{ route('login') }}">Profile</a>
-        <a class="active" href="{{ route('aboutUs') }}">About Us</a>
-        <a href="{{ route('contact') }}">Contact Us</a>
-        
+        <a class="active" href="{{ route('index') }}">Home</a>
 
+        <!-- Account Dropdown backend can you route this to the correct pages --> 
+        <div class="dropdown">
+            <a href="{{ route('shop') }}">Books</a>
+            <div class="dropdown-content">
+                <form action="{{ route('navigateShop') }}" method="POST">
+                    @csrf
+                    <input type="hidden" value="1" name="genre-select">
+                    <button type="submit">Fantasy</button>
+                </form>
+                <form action="{{ route('navigateShop') }}" method="POST">
+                    @csrf
+                    <input type="hidden" value="2" name="genre-select">
+                    <button type="submit">Horror</button>
+                </form>
+                <form action="{{ route('navigateShop') }}" method="POST">
+                    @csrf
+                    <input type="hidden" value="3" name="genre-select">
+                    <button type="submit">Mystery</button>
+                </form>
+                <form action="{{ route('navigateShop') }}" method="POST">
+                    @csrf
+                    <input type="hidden" value="4" name="genre-select">
+                    <button type="submit">Crime</button>
+                </form>
+                <form action="{{ route('navigateShop') }}" method="POST">
+                    @csrf
+                    <input type="hidden" value="5" name="genre-select">
+                    <button type="submit">Biography</button>
+                </form>
+            </div>
+        </div>
+        <a href="{{ route('aboutUs') }}">About Us</a>
+        <a href="{{ route('contact') }}">Contact Us</a>
         @if (Auth::check())
         <form action="{{ route('logout')}}" method="POST">
             @csrf
-        <button type="submit">Log Out</button>
+            <button type="submit">Log Out</button>
         </form>
-        @else 
-        <a href="{{ route('login') }}">Login</a>
-        </form>
+        @else
         @endif
     </div>
 
