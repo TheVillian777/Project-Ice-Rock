c74f0cfeae1f9339c119aea71bc3bc18471af94a
TheVillian777 <jaghayer89@gmail.com>
2025-03-12 11:36:35 +0000

    Min + Max Changes

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -70,7 +70,7 @@
         </div>
         <!-- price filter (sliding range) -->
          <div class="filter">
-            <label for="priceRange">Max Price:</label>
+            <label for="priceRange">Min Price:</label>
             <input type="range" id="priceRange" name="priceRange" min="0" max="25" value="{{ request()->input('priceRange', 0) }}">
             <span id="priceValue">£0</span>
         </div>
