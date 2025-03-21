8509db70cb3889d406f6062c8f141c627b86dc87
TheVillian777 <jaghayer89@gmail.com>
2025-03-21 18:19:58 +0000

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -116,4 +116,15 @@ class ProfileController extends Controller
         return view('reciepts', compact('orderitems','user_id','purchase_id'));
 
     }
+
+    public function returnItem(Request $request){
+
+        $purchase_id = request(['purchaseID']);
+        $book_id = request(['bookID']);
+
+        $user_id = Auth::id();
+
+        Purchase::where('purchase_id', $purchase_id)->where('book_id', $book_id)->limit(1)->update(['item_status' => 'returned']);
+
+    }
 }
