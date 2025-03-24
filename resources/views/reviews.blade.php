9b4dedcdc653d2707ef29e91b9b85e6f6a61046b
SamirMohamud <220170279@aston.ac.uk>
2025-03-24 04:44:08 +0000

    review
    tweaks

--- resources/views/reviews.blade.php
+++ resources/views/reviews.blade.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Reviews</title>
     <link rel="stylesheet" href="/css/reviews.css">
+    <link rel="stylesheet" href="/css/header.css">
+    
     <script src="js/reviews.js" defer></script>
     <script type="text/javascript" src="darkmode.js" defer></script>
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
