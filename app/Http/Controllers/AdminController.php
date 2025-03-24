8c20e15bb196e6145d0ef3c84a097228f425ffe2
Raihan <220111847@aston.ac.uk>
2025-03-24 07:12:39 +0000

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -103,6 +103,41 @@ class AdminController extends Controller
         } 
     }
 
+    public function register(Request $request)
+    {
+    //validate registration details
+    $user = $request->validate([
+        'title' => 'required',
+        'first_name' => 'required',
+        'last_name' => 'required',
+        'email' => 'required',
+        'phone' => 'nullable',
+        'password' => 'required',
+        'address' => 'required',
+        'security_answer' => 'required',
+        'confirm-password' =>  'required'
+    ]);
+
+    if ($user['password'] !== $request->input('confirm-password')) {
+      return redirect()->back()->withErrors('Password does not match');
+    }
+
+    //save registration to Users table
+    User::create([
+        'title' => $user['title'],
+        'first_name' => $user['first_name'],
+        'last_name' => $user['last_name'],
+        'email' => $user['email'],
+        'phone' => $user['phone'],
+        'isadmin' => false,
+        'address' => $user['address'],
+        'security_answer' => $user['security_answer'],
+        'password' => Hash::make($user['password']), //Hash for security with built in method
+    ]);
+
+    return redirect()->back()->with('Registration successful!');
+  }
+
 }
 
 
